c5e9e4bd9a608925a056cd6c0d92ed69a84212e4
Fernando <fernando@kidsnote.com>
2019-11-19 23:08:10 +0900

    scripts: 설치 스크립트 추가

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "============================================="
+echo "https://github.com/techinpark/xcode-snippets"
+echo "============================================="
+echo " "
+echo "📝 install xcode-snippets."; 
+cp *.codesnippet ~/Library/Developer/Xcode/UserData/CodeSnippets/
+echo "🚀 all snippets installed successfully. "
+echo "============================================="
